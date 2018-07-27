9cf3f649f9e7c2f1b3ef1dce8d2501948d167f77
Euan Ramsay <euancramsay@aol.com>
2018-07-27 14:46:58 +0100

    big: Fixed sign out.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,49 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { connect } from 'react-redux';
-import AppBar from '@material-ui/core/AppBar';
+import { connect } from 'react-redux'
+import AppBar from '@material-ui/core/AppBar'
 import User from './User'
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import { setAuthedUser } from '../actions/authedUser';
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import { setAuthedUser } from '../actions/authedUser'
 
 class Nav extends Component {
+  handleSignOut = () => {
+    const { dispatch } = this.props
+    dispatch(setAuthedUser(null))
+  }
 
-  render() {
+  render () {
     const { authedUser } = this.props
-    
+
     return (
-        
-        <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
-            <Button color="inherit">
-                <NavLink to='/' exact activeClassName='active'>
-                    Home
+          <Button color='inherit'>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </Button>
+          <Button color='inherit'>
+            <NavLink to='/new' activeClassName='active'>
+              New Question
+            </NavLink>
+          </Button>
+          <Button color='inherit'>
+            <NavLink to='/leaderboard' activeClassName='active'>
+              Leaderboard
+            </NavLink>
+          </Button>
+          {authedUser ? <User id={authedUser} /> : null}
+          {authedUser
+            ? <Button color='inherit' onClick={() => this.handleSignOut()}>
+              <NavLink to='/' activeClassName='active'>
+                  Sign Out
                 </NavLink>
             </Button>
-            <Button color="inherit">
-                <NavLink to='/new' activeClassName='active'>
-                    New Question
-                </NavLink>
-            </Button>
-            <Button color="inherit">
-                <NavLink to='/leaderboard' activeClassName='active'>
-                    Leaderboard
-                </NavLink>
-            </Button>
-            { authedUser 
-                ? <User id={authedUser}/>
-                : null } 
-            { authedUser 
-            ? <Button color="inherit" onClick="resetAuthedUser" >
-                <NavLink to='/' activeClassName='active'>
-                    Sign Out
-                </NavLink>
-            </Button>
-            : null } 
+            : null}
         </Toolbar>
-        </AppBar>
+      </AppBar>
     )
   }
 }
@@ -53,10 +54,4 @@ function mapStateToProps ({ authedUser }) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-    return({
-        resetAuthedUser: () => {dispatch(setAuthedUser(null))}
-    })
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Nav)
+export default connect(mapStateToProps)(Nav)
